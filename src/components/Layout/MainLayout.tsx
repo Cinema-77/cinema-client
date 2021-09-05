b07c63b09124edef7e355231c327624feacf6320
Tuyen <9a5.tuyen@gmail.com>
2021-09-05 15:30:48 +0700

    Add loop routes in Mainlayout

--- src/components/Layout/MainLayout.tsx
+++ src/components/Layout/MainLayout.tsx
@@ -1,11 +1,17 @@
+import { RouteWithSubRoutes } from '@/routes/config/routerConfig';
 import React from 'react';
+import { Switch } from 'react-router';
 import { Footer, Header } from '../Elements';
 
-export const MainLayout: React.FC = ({ children }) => {
+export const MainLayout: React.ReactNode = ({ routes }: any) => {
   return (
     <>
       <Header />
-      {children}
+      <Switch>
+        {routes.map((route: any, i: any) => (
+          <RouteWithSubRoutes key={i} {...route} />
+        ))}
+      </Switch>
       <Footer />
     </>
   );
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,5 @@
 export const path = {
   home: '/',
-  auth: 'auth',
+  auth: '/auth',
   notFound: '*',
 };
