a061b25219e02959844403074c3738b4715019f1
Tuyen <9a5.tuyen@gmail.com>
2021-09-07 13:54:21 +0700

    Fix UI Header + Footer

--- src/components/Elements/Header/Header.style.ts
+++ src/components/Elements/Header/Header.style.ts
@@ -1,36 +1,90 @@
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import BG from '@/assets/img/pattern-1.svg';
 
 export const Header = styled.header``;
+export const HeaderBG = styled.section`
+  background-color: #f5f5f5;
+`;
 export const HeaderTop = styled.section`
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
   gap: 0 4rem;
-  background-color: #f5f5f5;
-  padding: 1rem 0;
+  padding: 0.25rem 0;
 `;
 export const HeaderInfo = styled.ul`
   display: flex;
-  justify-content: center;
   align-items: center;
-  gap: 0 3rem;
-`;
-export const HeaderInfoItem = styled.li`
-  display: flex;
   justify-content: center;
-  align-items: center;
-  gap: 0 0.4rem;
-
+  gap: 0 2rem;
   svg {
-    width: 2.5rem;
-    height: 2.5rem;
+    width: 14px;
+    height: 14px;
   }
 `;
-export const HeaderInfoILink = styled(Link)`
+export const HeaderInfoSelect = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0 1rem;
+  color: #000;
+  font-weight: 500;
   font-size: 1.4rem;
-  line-height: 1.8rem;
+  padding: 8px 16px;
+
+  svg {
+    width: 0.8rem;
+    height: 0.8rem;
+    transform: rotate(-90deg);
+  }
+`;
+export const HeaderInfoSelectTitle = styled.h2``;
+export const HeaderInfoSelectList = styled.div`
+  display: none;
+  flex-direction: column;
+  position: absolute;
+  left: 0;
+  top: 100%;
+  background-color: #fff;
+  z-index: 1;
+`;
+export const HeaderInfoSelectWrapper = styled.div`
+  cursor: pointer;
+  position: relative;
+
+  &:hover ${HeaderInfoSelectList} {
+    display: flex;
+  }
+`;
+export const HeaderInfoSelectItem = styled.span`
+  padding: 0.8rem 1rem;
+  border: 1px solid #ccc;
+  font-size: 1.4rem;
+  line-height: 2.1rem;
+  color: #111;
+
+  &:first-child {
+    border-bottom: none;
+  }
+
+  &:hover {
+    background-color: #f5f5f5;
+  }
+`;
+
+export const HeaderInfoSpan = styled.span`
   text-transform: uppercase;
+  color: #000;
+  font-weight: 500;
+  font-size: 1.4rem;
+`;
+export const HeaderHelper = styled.section`
+  display: flex;
+`;
+export const HeaderHelperLink = styled(Link)`
+  padding: 8px 16px;
+  font-size: 1.4rem;
+  color: #000;
   font-weight: 500;
 
   &:hover {
@@ -41,73 +95,98 @@ export const HeaderAccount = styled.section`
   display: flex;
   justify-content: center;
   align-items: center;
-  gap: 0 0.4rem;
+  gap: 0 2rem;
 
   svg {
-    width: 2.5rem;
-    height: 2.5rem;
-  }
-
-  span {
+    width: 1.5rem;
+    height: 1.5rem;
     margin-bottom: 2px;
+    fill: #000;
   }
 `;
 export const HeaderAccountLink = styled(Link)`
-  font-size: 1.4rem;
+  font-size: 1.6rem;
   line-height: 1.8rem;
-  text-transform: uppercase;
-  font-weight: 500;
+  font-weight: 700;
 
   &:hover {
     color: red;
   }
 `;
-export const HeaderLanguage = styled.section`
-  color: #fff;
+export const HeaderSocial = styled.section`
+  display: flex;
+  gap: 0 2rem;
 `;
-export const HeaderLanguageVN = styled(Link)`
-  background-color: #e71a0f;
-  padding: 2px 8px;
-  border-radius: 0.5rem 0 0 0.5rem;
-`;
-export const HeaderLanguageEN = styled(Link)`
-  background-color: #898987;
-  padding: 2px 8px;
-  border-radius: 0 0.5rem 0.5rem 0;
+
+export const HeaderSocialLink = styled(Link)`
+  svg {
+    cursor: pointer;
+    width: 15px;
+    height: 15px;
+    fill: #909090;
+    font-size: 1.4rem;
+    line-height: 1.4rem;
+
+    &:hover {
+      fill: #303030;
+    }
+  }
 `;
 
 export const HeaderCenter = styled.section`
   display: flex;
-  align-items: flex-end;
+  align-items: center;
+  justify-content: space-between;
   gap: 0 4rem;
-  margin-top: 1rem;
+  margin-top: 2rem;
 `;
-export const HeaderLogo = styled(Link)``;
+export const HeaderLogo = styled(Link)`
+  font-size: 4rem;
+  color: #000;
+  font-weight: 700;
+`;
 export const HeaderList = styled.ul`
   display: flex;
   justify-content: center;
   align-items: center;
-  gap: 0 5rem;
+  gap: 0 0;
 `;
 export const HeaderSubList = styled.ul`
   position: absolute;
   display: none;
-  left: 0;
+  left: 50%;
   width: 100%;
   top: 100%;
-  transform: translateX(-10%);
-  color: #fff;
+  margin-top: 0px;
+  transform: translateX(-50%);
+  color: #505050;
   min-width: 190px;
   padding: 3px;
-  background: url('https://www.cgv.vn/skin/frontend/cgv/default/images/bg-cgv/bg_menu_hover.png');
+  border: 2px solid #ccc;
+  background-color: #fff;
+
+  &::before {
+    content: '';
+    position: absolute;
+    left: 0;
+    top: 0;
+    margin-top: -15px;
+    width: 100%;
+    height: 20px;
+  }
 `;
 export const HeaderItem = styled.li`
   position: relative;
   cursor: pointer;
-  color: #222222;
+  color: #000;
   font-weight: 700;
-  font-size: 1.4rem;
+  font-size: 1.6rem;
   line-height: 3rem;
+  padding: 8px 16px;
+
+  &:hover {
+    color: red;
+  }
 
   &:hover ${HeaderSubList} {
     display: block;
@@ -115,35 +194,24 @@ export const HeaderItem = styled.li`
 `;
 
 export const HeaderSubItem = styled.li`
-  border-left: 2px solid #828282;
-  border-right: 2px solid #828282;
   font-size: 1.4rem;
-  font-weight: 500;
-  padding: 0.5rem 1rem;
-
-  &:first-child {
-    border-top: 2px solid #828282;
-  }
-
-  &:last-child {
-    border-bottom: 2px solid #828282;
-  }
+  font-weight: 700;
+  padding: 0.5rem 2rem;
 
   &:hover {
     color: red;
   }
 `;
-export const HeaderOrderTicket = styled(Link)`
-  margin-left: auto;
-`;
 export const HeaderBottom = styled.section`
-  background-color: #000;
+  background-image: url(${BG});
+  background-color: #1f1f1f;
   color: #fff;
   font-weight: 500;
   font-size: 1.3rem;
+  letter-spacing: 0.65px;
   line-height: 1.95rem;
   text-align: center;
   text-transform: uppercase;
   margin-top: 2rem;
-  padding: 1.2rem 0;
+  padding: 1.4rem 0;
 `;
