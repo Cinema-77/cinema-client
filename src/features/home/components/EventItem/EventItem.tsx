5b161c1bc5315540542265c48f1306e4052290a6
Tuyen <9a5.tuyen@gmail.com>
2021-09-14 22:57:49 +0700

    hi

--- src/features/home/components/EventItem/EventItem.tsx
+++ src/features/home/components/EventItem/EventItem.tsx
@@ -4,6 +4,7 @@ import event1 from '@/assets/img/event 1.jpg';
 import event2 from '@/assets/img/event 2.jpg';
 import event3 from '@/assets/img/event 3.jpg';
 import event4 from '@/assets/img/event 4.jpg';
+//hihi
 
 export const EventItem = () => {
   const [listIMG] = useState([event1, event2, event3, event4]);
