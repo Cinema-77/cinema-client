e68c7d6423f539f401e17eb33ddb8008230d4be2
Tuyen <9a5.tuyen@gmail.com>
2021-12-18 12:57:44 +0700

    Update Coupon

--- src/features/account/components/Coupon/CouponItem.tsx
+++ src/features/account/components/Coupon/CouponItem.tsx
@@ -10,25 +10,29 @@ interface CouponItemProps {
 
 export const CouponItem: React.FC<CouponItemProps> = ({ item }) => {
   return (
-    <S.CouponItem>
-      <S.CouponItemLeft>
-        <S.CouponItemTitle>
-          <span>COUPON</span>
-          <span>{item.gift.name}</span>
-          <span>{item.code}</span>
-        </S.CouponItemTitle>
-        <S.CouponItemTitle>
-          Ngày hết hạn:
-          {' ' + moment(Number(item.dateExpiry)).format('MM-DD-YYYY')}
-        </S.CouponItemTitle>
-      </S.CouponItemLeft>
-      <S.CouponItemRight>
-        <h2>{item.gift.type === 1 ? 'COMBO' : 'TICKET'}</h2>
-        <S.CouponItemRightIMG>
-          100345
-          <img src={mavach} alt="" />
-        </S.CouponItemRightIMG>
-      </S.CouponItemRight>
-    </S.CouponItem>
+    <>
+      {Number(item.dateExpiry) > new Date().getTime() && (
+        <S.CouponItem>
+          <S.CouponItemLeft>
+            <S.CouponItemTitle>
+              <span>COUPON</span>
+              <span>{item.gift.name}</span>
+              <span>{item.code}</span>
+            </S.CouponItemTitle>
+            <S.CouponItemTitle>
+              Ngày hết hạn:
+              {' ' + moment(Number(item.dateExpiry)).format('MM-DD-YYYY')}
+            </S.CouponItemTitle>
+          </S.CouponItemLeft>
+          <S.CouponItemRight>
+            <h2>{item.gift.type === 1 ? 'COMBO' : 'TICKET'}</h2>
+            <S.CouponItemRightIMG>
+              100345
+              <img src={mavach} alt="" />
+            </S.CouponItemRightIMG>
+          </S.CouponItemRight>
+        </S.CouponItem>
+      )}
+    </>
   );
 };
